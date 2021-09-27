332107e3c3d14cffe7c0ee8080e26d8a2cd2fe1f
Pablo Seoane <pablitoseoane@gmail.com>
2021-09-27 18:27:29 -0300

    Subiendo Mercado Liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+/* Config public */
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'views')));
+
+
+/* Routes */
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+})
+
+
+app.listen(3000, () => console.log('Server corriendo en el puerto 3000'));
